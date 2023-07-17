abcdf3ab9b0a937c5602cb70ad348f1aa4254430
Prakash R <prakmyl@gmail.com>
2023-07-17 09:40:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   client_id       = "1e6a9095-67a4-4c91-b9f1-977ff15bc6e2" 
-  client_secret   = var.clientsecret
+  client_secret   = "BUJ8Q~Z0Vu3FW-KyLTOXNqTxBCdlusNBSFv4EbO8"
   subscription_id = "d194ddbd-6fc2-4bef-9091-802f670db6e3"
   tenant_id       = "27e7b34a-dacc-4659-8997-a9553f9be9db"
   features {}
