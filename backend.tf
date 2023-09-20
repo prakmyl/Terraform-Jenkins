7375fd10b2c29fc56f92ed2944f182fa858e105e
Prakash R <prakmyl@gmail.com>
2023-09-20 23:06:42 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "rmt-tf-state" # change this
-    key            = "prk/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
-}
