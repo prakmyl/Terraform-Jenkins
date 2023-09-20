d5aef71818705b1c4e86f8e3a83a9d0e4d84198b
Prakash R <prakmyl@gmail.com>
2023-09-20 19:41:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,131 +1,109 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
+
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired AWS region.
+}
+
+variable "cidr" {
+  default = "10.0.0.0/16"
+}
+
+resource "aws_key_pair" "example" {
+  key_name   = "terraform-demo-abhi"  # Replace with your desired key name
+  public_key = file("~/.ssh/id_rsa.pub")  # Replace with the path to your public key file
+}
+
+resource "aws_vpc" "myvpc" {
+  cidr_block = var.cidr
+}
+
+resource "aws_subnet" "sub1" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.myvpc.id
+}
+
+resource "aws_route_table" "RT" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
   }
 }
 
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-    client_id       =   var.client_id
-    client_secret   =   var.client_secret
-    subscription_id =   var.subscription_id
-    tenant_id       =   var.tenant_id
-  features {}
- 
+resource "aws_route_table_association" "rta1" {
+  subnet_id      = aws_subnet.sub1.id
+  route_table_id = aws_route_table.RT.id
 }
 
+resource "aws_security_group" "webSg" {
+  name   = "web"
+  vpc_id = aws_vpc.myvpc.id
 
-resource "azurerm_resource_group" "prk-rsrc" {
-  name     = "prk-resource"
-  location = "centralindia"
-  tags = {
-    environment = "dev"
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-resource "azurerm_virtual_network" "prk-nw" {
-  name                = "prk-network"
-  location            = azurerm_resource_group.prk-rsrc.location
-  resource_group_name = azurerm_resource_group.prk-rsrc.name
-  address_space       = ["10.0.0.0/16"]
-
-}
-
-resource "azurerm_subnet" "prk-sub" {
-  name                 = "prk-subnet"
-  resource_group_name  = azurerm_resource_group.prk-rsrc.name
-  virtual_network_name = azurerm_virtual_network.prk-nw.name
-  address_prefixes     = ["10.0.1.0/24"]
-
-}
-
-resource "azurerm_network_security_group" "prk-sg" {
-  name                = "prk-security-group"
-  location            = azurerm_resource_group.prk-rsrc.location
-  resource_group_name = azurerm_resource_group.prk-rsrc.name
-
-
-
-}
-resource "azurerm_network_security_rule" "prk-secrule" {
-  name                        = "prk-security-rule"
-  priority                    = 100
-  direction                   = "Outbound"
-  access                      = "Allow"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.prk-rsrc.name
-  network_security_group_name = azurerm_network_security_group.prk-sg.name
-}
-
-resource "azurerm_subnet_network_security_group_association" "prk-sgassoc" {
-  subnet_id                 = azurerm_subnet.prk-sub.id
-  network_security_group_id = azurerm_network_security_group.prk-sg.id
-}
-
-resource "azurerm_public_ip" "prk-pbip" {
-  name                = "prk-public-ip"
-  resource_group_name = azurerm_resource_group.prk-rsrc.name
-  location            = azurerm_resource_group.prk-rsrc.location
-  allocation_method   = "Dynamic"
-
-
-  tags = {
-    environment = "dev"
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-}
 
-resource "azurerm_network_interface" "prk-nic" {
-  name                = "prk-network-interface"
-  location            = azurerm_resource_group.prk-rsrc.location
-  resource_group_name = azurerm_resource_group.prk-rsrc.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.prk-sub.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.prk-pbip.id
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    environment = "dev"
+    Name = "Web-sg"
   }
 }
 
-resource "azurerm_linux_virtual_machine" "prk-vm" {
-
-  name                = "prk-ubuntu-vm"
-  resource_group_name = azurerm_resource_group.prk-rsrc.name
-  location            = azurerm_resource_group.prk-rsrc.location
-  size                = "Standard_D2s_v3"
-  admin_username      = "adminuser"
-  admin_password                  = "root@123"
-  disable_password_authentication = "false"
-  network_interface_ids = [ azurerm_network_interface.prk-nic.id ]
- 
-
- custom_data  = filebase64("${path.module}/customdata.tpl")
-
+resource "aws_instance" "server" {
+  ami                    = "ami-0261755bbcb8c4a84"
+  instance_type          = "t2.micro"
+  key_name      = aws_key_pair.example.key_name
+  vpc_security_group_ids = [aws_security_group.webSg.id]
+  subnet_id              = aws_subnet.sub1.id
   
-
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+  tags = {
+    Name = "falsk-app"
   }
 
-  source_image_reference {
-    publisher = "Canonical"
-     offer     = "UbuntuServer"
-    sku       = "18_04-lts-gen2"
-    version   = "latest"
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
+    private_key = file("~/.ssh/id_rsa")  # Replace with the path to your private key
+    host        = self.public_ip
+  }
 
+  # File provisioner to copy a file from local to the remote EC2 instance
+  provisioner "file" {
+    source      = "app.py"  # Replace with the path to your local file
+    destination = "/home/ubuntu/app.py"  # Replace with the path on the remote instance
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Hello from the remote instance'",
+      "sudo apt update -y",  # Update package lists (for ubuntu)
+      "sudo apt-get install -y python3-pip",  # Example package installation
+      "cd /home/ubuntu",
+      "sudo pip3 install flask",
+      "sudo python3 app.py &",
+    ]
   }
 }
- 
